0d919c9d16b56bac09c7e8afc51a6cb29314ad1e
Khusela Mphokeli <innothetechgeek@gmail.com>
2022-04-21 01:10:49 +0200

    minor changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -38,13 +38,17 @@ class UserController extends Controller
 
     public function login(Request $request){
 
-        $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $request->username)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response('Login invalid', 503);
         }
 
-        return $user->createToken($this->token_name);
+        return [
+            'token' => $user->createToken($this->token_name),
+            'user' => $this->retrieveUserImages(),
+        ];
+      
 
     }
 
@@ -53,12 +57,14 @@ class UserController extends Controller
 
         $token = $request->bearerToken();
 
+        //retrieve user based on the token provided
         $active_user = auth('sanctum')->user();
 
-        $active_user_id = $active_user->id;
+        $active_user_id = $active_user->id; //get user id
 
         $fileName = time().'_'.$request->file->getClientOriginalName();
-       
+        
+        //store the file in user azure storage disk
         $request->file('file')->storeAs("user_images/user_$active_user_id", $fileName, 'azure');
 
 
@@ -66,19 +72,23 @@ class UserController extends Controller
 
     public function retrieveUserImages(){
 
-        
+        //azure storage disk
          $disk = \Storage::disk('azure');
 
          $optedInUsers = User::where('OptIn',1)->get();
 
 
-         $user_images = array();
+      
          $users_arr = array();
 
          foreach($optedInUsers as $user){
 
             $user_id =  $user->id;
 
+            //array to hold user image links
+            $user_images = array();
+
+            //retrieve user files from azure storage disk
             $files = $disk->files("user_images/user_$user_id");
             
             if(count( $files )){
@@ -86,23 +96,23 @@ class UserController extends Controller
                 foreach($files as $file) {
                     
                     $azure_storage_url = config('app.azure_storage_url');
-
                     $image_link =  $azure_storage_url."/".$file;
 
+                    //store image link in users image array
                     array_push($user_images,$image_link);
                 }
 
                 $user->images = $user_images;
+                //store user object in the users array
                 array_push($users_arr, $user);
-               
 
             }
            
          }
-
-       
+         
          $results = json_encode($users_arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-  
+         
+         //returns response to the frontend
          return response($results)->header('content-type', 'application/json');
  
 
